5fe73cc08a94e7f4115d17867f1c93fa68e8254c
Jian-Hua, He <game09116@gmail.com>
2019-09-03 22:27:51 +0800

    Add items handler

--- coordinator/main.go
+++ coordinator/main.go
@@ -7,9 +7,24 @@ import (
 )
 
 func main() {
+	port := "80"
+
+	http.HandleFunc("/items", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			fmt.Println("Get counter")
+		case http.MethodPost:
+			fmt.Println("Post items")
+		default:
+			fmt.Println("Invalid method")
+		}
+	})
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Test")
 	})
 
-	log.Fatal(http.ListenAndServe(":80", nil))
+	log.Printf("Start coordinator at %s port", port)
+
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }

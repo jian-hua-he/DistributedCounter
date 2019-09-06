a90b80848295d84284c3031e417398705c0e453e
Jian-Hua, He <game09116@gmail.com>
2019-09-06 18:14:40 +0800

    Update test.sh

--- testclient/test.sh
+++ testclient/test.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
+## Test that won’t change the server state
+docker-compose up -d
+
 docker run -it --rm \
     -v $(PWD):/go/src/testclient \
     --network $(basename $(dirname $(PWD)))_dis_sys_network \
-    golang:1.12 bash -c "cd /go/src/testclient && go test ./..."
+    golang:1.12 bash -c "cd /go/src/testclient && go test api.go health_test.go none_item_test.go"
+
+docker-compose down
+
+# Test that would change the server state
+# Every tests should re-launch the services 
+docker-compose up -d
+
+docker run -it --rm \
+    -v $(PWD):/go/src/testclient \
+    --network $(basename $(dirname $(PWD)))_dis_sys_network \
+    golang:1.12 bash -c "cd /go/src/testclient && go test api.go multiple_items_test.go"
+
+docker-compose down
